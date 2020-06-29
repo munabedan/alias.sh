51816d56f0a3eae2ed86606ce873b7b1bcfe5b85
muna kimeria <45054928+munabedan@users.noreply.github.com>
2020-06-29 22:37:30 +0300

    added readme and bash_aliases file

---
+++ README.md
@@ -0,0 +1,17 @@
+# `alias.sh`
+
+A collection of various alias command applications.
+
+This project accompanies the article [`The alias command - Doing more by typing less`](https://web.archive.org/web/20190222132600/https://github.com/kitcambridge/evil.js) on my blog.
+
+## Contributions
+
+Suggestions and pull requests are welcome. Only helpful commands will be accepted , pranks and destructive aliases should be submitted to the appropriate media.
+
+## License
+
+This project is distributed under an [`MIT`](./LICENCE) license.
+
+## Disclaimer
+
+`alias.sh` is purely for informational purposes. I’m not responsible for anything you do with the commands.
---
+++ bash_aliases.sh
@@ -0,0 +1,30 @@
+# Below is a list of useful alias commands
+# Search a command in history
+alias gh='history | grep'
+
+# Find out which files you worked on last
+alias left='ls -t -1'
+
+# Shorten python3 to py
+alias py='python3'
+
+# Show todays
+alias today='date +"%A, %B %-d, %Y"'
+
+# Alias sudo
+alias sudo='sudo '
+
+# Update system
+alias sysupdate='apt update && apt upgrade'
+
+# Change cat to pr
+alias pr='cat'
+
+# Shorten jekyll commands
+alias jekylls='bundle exec jekyll serve'
+alias jekyllb='bundle exec jekyll build'
+alias jekylld='bundle exec jekyll serve --drafts'
+alias jekyllc='bundle exec jekyll clean'
+
+# Disable dd command
+alias dd='echo "sorry the dd command is disabled"'
